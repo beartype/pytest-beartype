4bcdd90ad158c7c4558230b2ad007ebf9f173187
Roman Knyazhitskiy <git@knyaz.tech>
2025-09-21 17:14:30 +0100

    Add tests for recursive fixtures

--- tests/pytest_tests/test_function_decoration.py
+++ tests/pytest_tests/test_function_decoration.py
@@ -26,6 +26,21 @@ def good_fixture() -> int:
     return 42
 
 
+@pytest.fixture
+def bad_recursive_fixture_first(bad_fixture) -> str:
+    return "banana"
+
+
+@pytest.fixture
+def bad_recursive_fixture_second(good_fixture: int) -> int:
+    return "banana"
+
+
+@pytest.fixture
+def good_recursive_fixture(good_fixture: int) -> int:
+    return good_fixture
+
+
 # Tests that should fail:
 
 
@@ -41,7 +56,18 @@ def test_using_good_fixture_wrong_annotation(good_fixture: str): ...
 def test_using_bad_fixture_wrong_annotation(bad_fixture: tuple): ...
 
 
+@pytest.mark.xfail(strict=True)
+def test_deep_fail_first(bad_recursive_fixture_first: str): ...
+
+
+@pytest.mark.xfail(strict=True)
+def test_deep_fail_second(bad_recursive_fixture_second: str): ...
+
+
 # Tests that should succeed:
+
+
+def test_deep_success(good_recursive_fixture: int): ...
 
 
 def test_using_good_fixture_good_annotation(good_fixture: int): ...
